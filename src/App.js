f41cfa25e16d2b40fec0d4f9f1dc7596fd376c95
chybatronik <chybatron@gmail.com>
2017-10-20 16:09:03 +0300

    add width and height

--- src/App.js
+++ src/App.js
@@ -5,11 +5,14 @@ import Sortable from './Sortable';
 
 const springConfig = {stiffness: 300, damping: 50};
 
+let step_y = 90
+let step_x = 90
+let delta = 5
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.sortable = new Sortable();
+    this.sortable = new Sortable(step_x, step_y, delta);
     console.log("sortable::", this.sortable.get_state())
 
     this.state = this.sortable.get_state();
@@ -42,8 +45,9 @@ class App extends Component {
   handleMouseDown = (pos, [pressX, pressY], {pageX, pageY}) => {
     this.sortable.handleMouseDown(pos, [pressX, pressY], {pageX, pageY})
     let st = this.sortable.get_state()
+    console.log("pos", pos, st)
     this.setState({
-      lastPress: st.lastPress,
+      lastPress: pos,
       isPressed: st.isPressed,
       mouseY: st.mouseY,
       mouseX: st.mouseX
@@ -60,48 +64,55 @@ class App extends Component {
 
   render() {
     const {order, lastPress, isPressed, mouseX, mouseY} = this.state;
+    // console.log("lastPress", lastPress)
     let result = []
-    order.forEach((_, key_y) => {
-      order[key_y].forEach((currentValue, key) => {
-        let style;
-        if (currentValue === lastPress && isPressed) {
-          style = {
-            scale: spring(1.3, springConfig),
-            shadow: spring(1.3, springConfig),
-            y: mouseY,
-            x: mouseX,
-          }
-        }else{
-          style = {
-            scale: spring(1, springConfig),
-            shadow: spring(1, springConfig),
-            y: spring(key_y * 100, springConfig),
-            x: spring(key * 100, springConfig),
-          }
+    // let st = this.sortable.get_state()
+    order.forEach((value, key) => {
+      // order[key_y].forEach((currentValue, key) => {
+      let style;
+      if (lastPress && value.id === lastPress.id && isPressed) {
+        style = {
+          scale: spring(1.2, springConfig),
+          shadow: spring(1.2, springConfig),
+          y: mouseY,
+          x: mouseX,
         }
-        result.push((
-          <Motion key={currentValue} style={style}>
-            {({scale, shadow, y, x}) =>
-              <div
-                onMouseDown={this.handleMouseDown.bind(null, currentValue, [x, y])}
-                onTouchStart={this.handleTouchStart.bind(null, currentValue, [x, y])}
-                className="demo2-ball"
-                style={{
-                  boxShadow: `rgba(0, 0, 0, 0.2) 0px ${shadow}px ${2 * shadow}px 0px`,
-                  transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`,
-                  WebkitTransform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`,
-                  zIndex: currentValue === lastPress ? 99 : currentValue,
-                }}>
-                {currentValue}
-              </div>
-            }
-          </Motion>
-        ));
-      })
+      }else{
+        style = {
+          scale: spring(1, springConfig),
+          shadow: spring(1, springConfig),
+          y: spring(value.y, springConfig),
+          x: spring(value.x, springConfig),
+        }
+      }
+      result.push((
+        <Motion key={value.id} style={style}>
+          {({scale, shadow, y, x}) =>
+            <div
+              onMouseDown={this.handleMouseDown.bind(null, value, [x, y])}
+              onTouchStart={this.handleTouchStart.bind(null, value, [x, y])}
+              className="demo-item"
+              style={{
+                // top:45,
+                // left:45,
+                width: value.width,
+                height: value.height,
+                boxShadow: `rgba(0, 0, 0, 0.2) 0px ${shadow}px ${2 * shadow}px 0px`,
+                transform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`,
+                WebkitTransform: `translate3d(${x}px, ${y}px, 0) scale(${scale})`,
+                zIndex: lastPress && value.id === lastPress.id ? 99 : value.id,
+                // opacity: lastPress && value.id === lastPress.id ? 0.7 : 1,
+              }}>
+              {value.id}
+            </div>
+          }
+        </Motion>
+      ));
+      // })
     })
     return (
-      <div  className="demo8-outer ">
-        <div className="demo2">
+      <div  className="demo-outer ">
+        <div className="demo">
           {result}
         </div>
       </div>
--- src/Sortable.js
+++ src/Sortable.js
@@ -1,16 +1,39 @@
-
 class Sortable{
-  constructor(){
+  constructor(step_x, step_y, delta){
     this.state = {
+      delta: delta,
+      step_x:step_x,
+      step_y:step_y,
       mouseX: 0,
       mouseY: 0,
       lastPress: null,
       isPressed: false,
-      order: [
-        [1, 2, 3, 4, 5, 6],
-        [11, 12, 13, 14, 15, 16],
-        [21, 22, 23, 24, 25, 26]
-      ],
+      currentRow:null,
+      currentCol:null,
+      // order: [
+      //   [1, 2, 3, 4, 5, 6],
+      //   [11, 12, 13, 14, 15, 16],
+      //   [21, 22, 23, 24, 25, 26]
+      // ],
+      order:[
+        {id: 1, w:1, h:1, col:1, row:1},
+        {id: 2, w:1, h:1, col:2, row:1},
+        {id: 3, w:1, h:1, col:3, row:1},
+        {id: 4, w:1, h:1, col:4, row:1},
+        {id: 5, w:2, h:2, col:5, row:1},
+        {id: 6, w:1, h:1, col:6, row:1},
+        {id: 7, w:1, h:1, col:7, row:1},
+        {id: 8, w:1, h:1, col:8, row:1},
+        {id: 9, w:1, h:1, col:9, row:1},
+
+        {id: 11, w:1, h:1, col:1, row:2},
+        {id: 12, w:1, h:1, col:2, row:2},
+        {id: 13, w:1, h:1, col:3, row:2},
+
+        {id: 21, w:1, h:1, col:1, row:3},
+        {id: 22, w:1, h:1, col:2, row:3},
+        {id: 23, w:1, h:1, col:3, row:3},
+      ]
     };
   }
 
@@ -19,57 +42,165 @@ class Sortable{
   }
 
   get_state(){
-    return this.state
+    // let temp = this.state
+    let res_order = []
+    let copy = Object.assign({}, this.state);
+    copy.order.forEach((value, key) => {
+      let ttm = value
+      ttm.width = ttm.w * copy.step_x + (ttm.w-1)*copy.delta
+      ttm.height = ttm.h * copy.step_y + (ttm.h-1)*copy.delta
+      ttm.y = ttm.row * (copy.step_y + copy.delta)
+      ttm.x = ttm.col * (copy.step_x + copy.delta)
+      res_order.push(ttm)
+    })
+    copy.order = res_order
+
+    return copy
   }
 
   handleMouseMove({pageX, pageY}){
-    const {isPressed, topDeltaY, topDeltaX, order, lastPress} = this.state;
+    const {isPressed, topDeltaY, topDeltaX, lastPress} = this.state;
 
     if (isPressed) {
-      const mouseY = pageY -topDeltaY ;
-      const currentRow = this.clamp(Math.round(mouseY / 100), 0, 10);
-      console.log("currentRow", currentRow)
+      let copy = Object.assign({}, this.state);
+      const mouseY = pageY - topDeltaY ;
+      const currentRow = this.clamp(Math.round(mouseY / this.state.step_y), 1, 10);
+
+      // console.log("currentRow", currentRow)
       // let newOrder = order;
 
-      const mouseX = pageX-topDeltaX;
-      const currentCol = this.clamp(Math.round(mouseX / 100), 0, 10);
-      console.log("currentCol", currentCol)
-      let result = []
-      console.log("order[currentRow][currentCol]", order[currentRow][currentCol])
+      const mouseX = pageX - topDeltaX;
+      const currentCol = this.clamp(Math.round(mouseX / this.state.step_x), 1, 10);
+      // console.log("currentCol", currentCol)
+      // let result = []
+      // console.log("order[currentRow][currentCol]", order[currentRow][currentCol])
       let new_row = []
-      order.forEach((_, key_y) => {
-        if(key_y === currentRow){
-          new_row = []
-          order[key_y].forEach((current, key) => {
-            if(currentCol === key){
+      let last_col = null
+      let last_row = null
+      let  w = null
+      let h = null
+      copy.order.forEach((value, key_y) => {
+        if(lastPress.id===value.id){
+          let cp_value = Object.assign({}, value);
 
-              new_row.push(lastPress)
+          last_col = cp_value.col
+          last_row = cp_value.row
+          // width = cp_value.w * copy.step_x + (cp_value.w-1)*copy.delta
+          // height = cp_value.h * copy.step_y + (cp_value.h-1)*copy.delta
+          w = cp_value.w
+          h = cp_value.h
+          // console.log("w", w)
+          // console.log("h", h)
 
-            }else{
-              if(lastPress===current){
-                new_row.push(order[currentRow][currentCol])
+          cp_value.col = currentCol
+          cp_value.row = currentRow
+
+          new_row.push(cp_value)
+        }
+
+      })
+      copy.order.forEach((value, key_y) => {
+        // console.log("value", value
+        if(lastPress.id!==value.id){
+          let cp_value = Object.assign({}, value);
+          if(value.col === currentCol && value.row === currentRow){
+            console.log("========",  value.id)
+            if(currentCol >= this.state.currentCol){
+              console.log("cp_value.col = last_col")
+              cp_value.col = last_col
+              if(currentRow > this.state.currentRow){
+                console.log("cp_value.row = last_row")
+                cp_value.row = last_row
               }else{
-                new_row.push(current)
+                cp_value.row = cp_value.row + (h)
+                console.log("cp_value.row = cp_value.row + (h)")
+              }
+            }else{
+              console.log("cp_value.col = cp_value.col + (w)")
+              cp_value.col = cp_value.col + (w)
+              if(currentRow > this.state.currentRow){
+                console.log("cp_value.row = last_row")
+                cp_value.row = last_row
               }
             }
-          })
-          result.push(new_row)
-        }else{
-          new_row = []
-          order[key_y].forEach((current, key) => {
-            if(lastPress===current){
-              new_row.push(order[currentRow][currentCol])
-            }else{
-              new_row.push(current)
+
+            // if(currentRow > this.state.currentRow){
+            //   console.log("cp_value.row = last_row")
+            //   cp_value.row = last_row
+            // }else{
+            //   cp_value.row = cp_value.row + (h)
+            //   console.log("cp_value.row = cp_value.row + (h)")
+            // }
+          }else{
+
+            if(currentCol > this.state.currentCol && value.row >= currentRow && value.row <= (currentRow + (h-1))){
+              if(value.col >= currentCol){
+                console.log("Col>>>>>>>",  value.id)
+                cp_value.col = cp_value.col + (w-1)
+              }
             }
-          })
-          result.push(new_row)
+            // console.log("ser::", value.id, value.col)
+            if(currentCol < this.state.currentCol && value.row >= currentRow && value.row <= (currentRow + (h-1))){
+              if(value.col >= (currentCol) && value.col <= (currentCol+w)){
+                console.log("Col<<<<<<<", value.id)
+                cp_value.col = cp_value.col + (w)
+              }
+            }
+            ////////////////////////////////////////////////////////
+            // for rows
+            if(currentRow > this.state.currentRow  && value.col >= currentCol && value.col <= (currentCol + (w-1))){
+              if(value.row >= currentRow){
+                console.log("Row++++++++",  value.id)
+                cp_value.row = cp_value.row + (h-1)
+              }
+            }
+
+            if(currentRow < this.state.currentRow && value.col >= currentCol && value.col <= (currentCol + (w-1))){
+              if(value.row >= (currentRow) && value.row < (currentRow+h)){
+                console.log("Row--------", value.id)
+                cp_value.row = cp_value.row + (h)
+              }
+            }
+            // if(value.col >= currentCol && value.col <= (currentCol + (w-1)) && value.row >= currentRow && value.row <= (currentRow + (h-1)) ){
+            //   console.log("currentRow", currentRow)
+            //   console.log("currentCol", currentCol)
+            //   console.log("cp_value", cp_value)
+            //   // console.log("cp_value.row", cp_value.row)
+            //   console.log("width", w)
+            //   console.log("height", h)
+            //   console.log("state", this.state)
+            //   if(currentCol > this.state.currentCol){
+            //     cp_value.col = cp_value.col + (w-1)
+            //   }
+            //   if(currentCol < this.state.currentCol){
+            //     cp_value.col = cp_value.col + (w+1)
+            //   }
+            //   // if(currentRow !== this.state.currentRow){
+            //   //   cp_value.row = cp_value.row + (h-1)
+            //   // }
+            // }
+            // if(value.row > currentRow && value.col >= (currentCol + (w-1))){
+            //   if(currentRow !== this.state.currentRow){
+            //     cp_value.row = cp_value.row + (h-1)
+            //   }
+            //   // cp_value.row = last_row
+            // }
+          }
+          new_row.push(cp_value)
         }
+
       })
-      console.log("result", result)
+      // console.log("result", result)
+      if(currentRow !== this.state.currentRow){
+        this.state.currentRow = currentRow
+      }
+      if(currentCol !== this.state.currentCol){
+        this.state.currentCol = currentCol
+      }
       this.state.mouseY = mouseY
       this.state.mouseX = mouseX
-      this.state.order = result
+      this.state.order = new_row
+
     }
   }
 
@@ -78,10 +209,11 @@ class Sortable{
     this.state.isPressed = true
     this.state.mouseY = pressY
     this.state.mouseX = pressX
+    this.state.old_order = this.state.order
     this.state.topDeltaY = pageY - pressY
     this.state.topDeltaX = pageX - pressX
   }
-  
+
   handleMouseUp(){
     this.state.isPressed = false
     this.state.topDeltaY = 0
